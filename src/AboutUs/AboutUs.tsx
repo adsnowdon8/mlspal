7ca60294b60c7280ac04cf82f29740f6955c2838
Adam <adsnowdon@gmail.com>
2025-07-03 02:45:37 -0400

    about us update

--- src/AboutUs/AboutUs.tsx
+++ src/AboutUs/AboutUs.tsx
@@ -12,9 +12,10 @@ const AboutUs = () => {
         Next Pro soccer. We are currently working on gathering and incorporating
         key performance data into both our Young Player and MLS Player databases
         and hope to roll out an updated version with these metrics soon. We
-        encourage you to reach out to us at blahblahblah@gmail.com or
-        703-623-7320 if you have any questions, suggestions or would like to
-        collaborate on a specific project.
+        encourage you to reach out to us at{" "}
+        <a href="mailto:hi@mlspal.app">hi@mlspal.app</a> if you have any
+        questions, suggestions or would like to collaborate on a specific
+        project.
       </p>
       <p className="mt-3 indent-4">
         Thank you for choosing MLS PAL. Please share with other MLS fans and
